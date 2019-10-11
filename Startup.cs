9c35bd34e188f854d543155b58f477656f816864
liguang <liguang@vlinker.com.cn>
2019-10-11 16:15:06 +0800

    dd

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace Test
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            else   
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
